ad5d65dadd7db7cefa420e0133535ff36c3ea538
Alberto Padovan <alberto.padovan.94@gmail.com>
2025-03-25 20:41:28 -0500

    Fix bug in __init__.py after deleting some operators

--- resolvent4py/linear_operators/__init__.py
+++ resolvent4py/linear_operators/__init__.py
@@ -3,7 +3,6 @@ __all__ = [
     "LowRankLinearOperator",
     "LowRankUpdatedLinearOperator",
     "ProductLinearOperator",
-    "InputOutputLinearOperator",
     "LinearOperator"
 ]
 
@@ -12,5 +11,3 @@ from .matrix import MatrixLinearOperator
 from .low_rank import LowRankLinearOperator
 from .low_rank_updated import LowRankUpdatedLinearOperator
 from .product import ProductLinearOperator
-from .input_output import InputOutputLinearOperator
-from .projected import ProjectedLinearOperator
