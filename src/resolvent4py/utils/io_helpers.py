6ca0a0f7a48b144b7e789e29e77dad4f28003c99
Alberto Padovan <alberto.padovan.94@gmail.com>
2025-04-21 20:53:00 -0400

    Add read_harmonic_balanced_bv() in io_helpers.py. Not yet verified

--- src/resolvent4py/utils/io_helpers.py
+++ src/resolvent4py/utils/io_helpers.py
@@ -6,6 +6,7 @@ from .. import typing
 
 from .mat_helpers import convert_coo_to_csr
 from .comms_helpers import compute_local_size
+from .bv_helpers import bv_conj
 
 
 def read_vector(
@@ -119,6 +120,9 @@ def read_harmonic_balanced_matrix(
     :param full_sizes: sizes :code:`((local rows, global rows), 
         (local cols, global cols))` of :math:`M`
     :type full_sizes: Tuple[Tuple[int, int], Tuple[int, int]]
+
+    :return: Sparse PETSc matrix :math:`M`
+    :rtype: PETSc.Mat
     """
     # Read list of COO vectors
     rows_lst, cols_lst, vals_lst = [], [], []
@@ -155,7 +159,7 @@ def read_harmonic_balanced_matrix(
     nblocks = full_sizes[0][-1]//Nrb    # Number of blocks
     nfb = (len(rows_lst) - 1) // 2  # Number of baseflow frequencies
     nfp = (nblocks - 1) // 2        # Number of perturbation frequencies
-    if nfb < nfb:
+    if nfp < nfb:
         raise ValueError (
             f"The number of blocks must be larger than the number of Fourier "
             f"coefficients of A(t). (See function description.)"
@@ -235,6 +239,94 @@ def read_bv(
     M = SLEPc.BV().createFromMat(Mm)
     M.setType("mat")
     Mm.destroy()
+    return M
+
+
+def read_harmonic_balanced_bv(
+    comm: MPI.Comm,
+    filenames_lst: typing.List[str],
+    real_bflow: bool,
+    block_sizes: typing.Tuple[typing.Tuple[int, int], int],
+    full_sizes: typing.Tuple[typing.Tuple[int, int], int],
+) -> SLEPc.BV:
+    r"""
+    Given :math:`\{\ldots, A_{-1}, A_{0}, A_{1},\ldots\}`, where :math:`A_j` is
+    the :math:`j` th Fourier coefficient of a time-periodic matrix :math:`A(t)`,
+    assemble the harmonic-balanced matrix
+
+    .. math::
+
+        M = \begin{bmatrix}
+        \ddots & \ddots & \ddots & \ddots \\
+        \ddots & A_{0} & A_{-1} & A_{-2} & \ddots \\
+        \ddots & A_{1} & A_{0} & A_{-1} & A_{-2} & \ddots \\
+        \ddots & A_{2} & A_{1} & A_{0} & A_{-1} & A_{-2} &\ddots \\
+        & \ddots & A_{2} & A_{1} & A_{0} & A_{-1} &\ddots \\
+        &  & \ddots & A_{2} & A_{1} & A_{0} & \ddots\\
+        & & & \ddots & \ddots & \ddots & \ddots \\
+        \end{bmatrix}
+    
+    :param comm: MPI communicator (MPI.COMM_WORLD)
+    :type comm: MPI.Comm
+    :param filenames_lst: list of names of files where the Fourier modes 
+        :math:`A_j` are stored as SLEPc BVs
+    :type filenames_lst: List[str]
+    :param real_bflow: set to :code:`True` if :code:`filenames_lst` 
+        contains the names of the COO arrays of the positive Fourier 
+        coefficients of :math:`A(t)` (i.e., :math:`\{A_0, A_1, A_2, \ldots\}`). 
+        The negative frequencies are assumed the complex-conjugates of the 
+        positive ones. Set to :code:`False` otherwise
+        (i.e., :math:`\{\ldots, A_{-2}, A_{-1}, A_0, A_1, A_2, \ldots\}`).
+    :type real_bflow: bool
+    :param block_sizes: sizes :code:`((local rows, global rows), global_cols)` 
+        of :math:`A_j`
+    :type block_sizes: Tuple[Tuple[int, int], int]
+    :param full_sizes: sizes :code:`((local rows, global rows), global_cols)` 
+        of :math:`M`
+    :type full_sizes: Tuple[Tuple[int, int], int]
+
+    :return: SLEPc BV :math:`M`
+    :rtype: SLEPc.BV
+    """
+    # Read list of COO vectors
+    bvs_lst = []
+    for filename in filenames_lst:
+        bv = read_bv(comm, filename, block_sizes)
+        bvs_lst.append(bv.copy())
+        bv.destroy()
+
+    if real_bflow:
+        l = len(bvs_lst)
+        for i in range(1, l):
+            idx_lst = i - l
+            bvs_lst.insert(0, bv_conj(bvs_lst[idx_lst]))
+    
+    Nrb = block_sizes[0][-1]    # Number of rows for each block
+    Ncb = block_sizes[-1]       # Number of cols for each block
+    nblocks = full_sizes[0][-1]//Nrb    # Number of blocks
+    nfb = (len(bvs_lst) - 1) // 2       # Number of baseflow frequencies
+    nfp = (nblocks - 1) // 2            # Number of perturbation frequencies
+    if nfp < nfb:
+        raise ValueError (
+            f"The number of blocks must be larger than the number of Fourier "
+            f"coefficients of A(t). (See function description.)"
+        )
+    M = SLEPc.BV().create(comm)
+    M.setSizes(full_sizes)
+    M.setType('mat')
+    Mmat = M.getMat()
+    r0, r1 = Mmat.getOwnershipRange()
+    for i in range(2 * nfp + 1):
+        for j in range(2 * nfp + 1):
+            k = i - j + nfb
+            if k >= 0 and k < (2 * nfp + 1):
+                rows = np.arange(r0, r1) + i * Nrb
+                cols = np.arange(Ncb) + j * Ncb
+                bv_k_mat = bvs_lst[k].getMat()
+                Mmat.setValues(rows, cols, bv_k_mat.getDenseArray(), None)
+                bvs_lst[k].restoreMat(bv_k_mat)
+    Mmat.assmble(None)
+    M.restoreMat(Mmat)
     return M
 
 
