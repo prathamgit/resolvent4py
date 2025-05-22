387cccb7ca4d8652e2c5a2c2ebd6dfd0466f553c
Alberto Padovan <alberto.padovan.94@gmail.com>
2025-05-21 23:50:04 -0400

    Clean up some commented out stuff

--- src/resolvent4py/linear_operators/matrix.py
+++ src/resolvent4py/linear_operators/matrix.py
@@ -43,24 +43,6 @@ class MatrixLinearOperator(LinearOperator):
         y = self.create_left_vector() if y == None else y
         self.A.mult(x, y)
         return y
-
-    # def hermitian_transpose(self):
-    #     self.A.hermitianTranspose()
-
-    # def axpy(self, alpha, X, Y=None):
-    #     Xm = X.A
-    #     if Y != None:
-    #         Ym = Y.A
-    #         Ym.axpy(alpha, Xm)
-    #         Y.restoreMat(Ym)
-    #     else:
-    #         Ym = Xm.duplicate()
-    #         Ym.axpy(alpha, Xm)
-    #         Y = SLEPc.BV().createFromMat(Ym)
-    #         Y.setType("mat")
-    #         Ym.destroy()
-    #     return Y
-
     
     def apply_mat(self, X, Y=None):
         Xm = X.getMat()
@@ -76,10 +58,7 @@ class MatrixLinearOperator(LinearOperator):
             Ym.destroy()
         X.restoreMat(Xm)
         return Y
-        # Y = self.A.matMult(X, Y)
-        # return Y
-
-
+    
     def apply_hermitian_transpose(self, x, y=None):
         y = self.create_right_vector() if y == None else y
         self.A.multHermitian(x, y)
